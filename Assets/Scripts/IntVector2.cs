df0d1c4b1e4c012f846238e64b04997717222791
John Luke Saake <johnlukesaake@gmail.com>
2018-02-18 14:01:57 -0800

    Generation Randomness Introduced
    Start work on random / directional generation of maze cells

---
+++ Assets/Scripts/IntVector2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct IntVector2{
+
+    public int x, z;
+
+    public IntVector2 (int _x, int _z)
+    {
+        this.x = _x;
+        this.z = _z;
+    }
+
+    public static IntVector2 operator + (IntVector2 a, IntVector2 b)
+    {
+        a.x += b.x;
+        a.z += b.z;
+        return a;
+    }
+}
--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MazeCell : MonoBehaviour {
 
+    public IntVector2 coordinates;
+
 	// Use this for initialization
 	void Start () {
 		
