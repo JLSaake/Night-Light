2374b16b70f8c33e509ea2f24cb936d878029843
John Luke Saake <johnlukesaake@gmail.com>
2018-02-20 13:31:02 -0800

    Campfire
    Campfire mechanics working (passive generation as player stands around)

--- Assets/Scripts/Campfire.cs
+++ Assets/Scripts/Campfire.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Campfire : MonoBehaviour {
 
+    public float cooldown;
+    public int lightGive;
+
+    private bool onCooldown;
+
 	// Use this for initialization
 	void Start () {
-		
+        onCooldown = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && !onCooldown)
+        {
+            StartCoroutine(GiveLight(lightGive));
+        }
+    }
+
+    private IEnumerator GiveLight(int light)
+    {
+        onCooldown = true;
+        Scoring.AddLight(light);
+        Debug.Log("Warming! " + Scoring.GetLight() + " light remaining");
+        yield return new WaitForSeconds(cooldown);
+        onCooldown = false;
+    }
 }
